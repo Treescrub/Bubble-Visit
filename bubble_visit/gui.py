00d254bba06e14ae83693a61ca9c949402b31e19
Treescrub <treescrub@gmail.com>
2024-02-27 02:01:18 -0700

    Implement basic GUI

--- bubble_visit/gui.py
+++ bubble_visit/gui.py
@@ -6,4 +6,31 @@ VERSION = "0.1.0"
 
 
 def run():
-    pass
+    window = Tk()
+    window.title("Bubble Visit")
+    window.option_add("*tearOff", FALSE)
+
+    frame = ttk.Frame(window, padding=10)
+    frame.pack()
+
+    system_count_frame = ttk.Frame(frame, padding=10)
+    system_count_frame.pack()
+
+    sector_sys_label = ttk.Label(system_count_frame, text="Visited systems in sector: 0")
+    sector_sys_label.pack()
+
+    bounds_sys_label = Label(system_count_frame, text="Visited systems in bounds: 0")
+    bounds_sys_label.pack()
+
+    button = ttk.Button(frame, text="Complete sector")
+    button.pack()
+
+    menubar = Menu(window)
+    window["menu"] = menubar
+
+    menu_file = Menu(menubar)
+    menu_file.add_command(label="Test")
+
+    menubar.add_cascade(menu=menu_file, label="File")
+
+    window.mainloop()
