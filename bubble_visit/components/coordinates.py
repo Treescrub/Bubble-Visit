43b8dc338a4f015b0f3ffd986700bcd04a375fab
Treescrub <treescrub@gmail.com>
2024-03-03 21:59:54 -0700

    Refactor components to allow caller to use pack or grid

--- bubble_visit/components/coordinates.py
+++ bubble_visit/components/coordinates.py
@@ -1,16 +1,13 @@
 from tkinter import ttk
 
 
-class SectorCoordinates(ttk.Frame):
+class SectorCoordinates(ttk.LabelFrame):
     def __init__(self, parent):
-        super().__init__()
+        super().__init__(parent, text="Coordinates", padding=10)
 
-        coordinate_frame = ttk.LabelFrame(parent, text="Coordinates", padding=10)
-        coordinate_frame.pack()
-
-        ttk.Label(coordinate_frame, text="X", padding=5).grid(row=0, column=0)
-        ttk.Entry(coordinate_frame, width=5).grid(row=0, column=1)
-        ttk.Label(coordinate_frame, text="Y", padding=5).grid(row=1, column=0)
-        ttk.Entry(coordinate_frame, width=5).grid(row=1, column=1)
-        ttk.Label(coordinate_frame, text="Z", padding=5).grid(row=2, column=0)
-        ttk.Entry(coordinate_frame, width=5).grid(row=2, column=1)
+        ttk.Label(self, text="X", padding=5).grid(row=0, column=0)
+        ttk.Entry(self, width=5).grid(row=0, column=1)
+        ttk.Label(self, text="Y", padding=5).grid(row=1, column=0)
+        ttk.Entry(self, width=5).grid(row=1, column=1)
+        ttk.Label(self, text="Z", padding=5).grid(row=2, column=0)
+        ttk.Entry(self, width=5).grid(row=2, column=1)
--- bubble_visit/components/system_counts.py
+++ bubble_visit/components/system_counts.py
@@ -1,15 +1,12 @@
 from tkinter import ttk
 
 
-class SystemCounts(ttk.Frame):
+class SystemCounts(ttk.LabelFrame):
     def __init__(self, parent):
-        super().__init__(parent)
+        super().__init__(parent, text="Systems", padding=10)
 
-        system_count_frame = ttk.LabelFrame(parent, text="Systems", padding=10)
-        system_count_frame.pack()
+        sector_sys_label = ttk.Label(self, text="Visited systems in sector: 0")
+        sector_sys_label.grid(row=0, column=0)
 
-        sector_sys_label = ttk.Label(system_count_frame, text="Visited systems in sector: 0")
-        sector_sys_label.pack()
-
-        bounds_sys_label = ttk.Label(system_count_frame, text="Visited systems in bounds: 0")
-        bounds_sys_label.pack()
+        bounds_sys_label = ttk.Label(self, text="Visited systems in bounds: 0")
+        bounds_sys_label.grid(row=1, column=0)
